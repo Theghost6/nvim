8990eded6c6d472b9442746330d813ac0dd288f3
Theghost6 <nguyensinh15082004@gmail.com>
2024-04-08 21:28:41 +0700

    update

--- lua/z/plugins/code_runer.lua
+++ lua/z/plugins/code_runer.lua
@@ -2,6 +2,25 @@ return {
 	"CRAG666/code_runner.nvim",
 	config = function()
 		require("code_runner").setup({
+      mode = 'float',
+			float = {
+				close_key = "<ESC>",
+				-- Window border (see ':h nvim_open_win')
+				border = "single",
+
+				-- Num from `0 - 1` for measurements
+				height = 0.8,
+				width = 0.8,
+				x = 0.5,
+				y = 0.5,
+
+				-- Highlight group for floating window/border (see ':h winhl')
+				border_hl = "Normal",
+				float_hl = "Normal",
+
+				-- Transparency (see ':h winblend')
+				blend = 0,
+			},
 			filetype = {
 				java = {
 					"cd $dir &&",
--- lua/z/plugins/toggleterm.lua
+++ lua/z/plugins/toggleterm.lua
@@ -1,4 +1,25 @@
 return {
 	-- amongst your other plugins
-	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+				close_on_exit = true,
+				direction = "float",
+				-- float_opts = {
+				-- 	border = "single", -- Kiểu đường viền, có thể là "single", "double", "shadow", "rounded", hoặc "none"
+				-- 	width = 90, -- Chiều rộng của cửa sổ floating
+				-- 	height = 0.9, -- Chiều cao của cửa sổ floating, tính theo phần trăm của chiều cao màn hình
+				-- 	winblend = 3, -- Độ mờ của cửa sổ floating, giá trị từ 0 (không mờ) đến 100 (hoàn toàn mờ)
+				-- 	highlights = {
+				-- 		border = "Normal", -- Hiệu ứng màu sắc của đường viền
+				-- 		background = "Normal", -- Màu nền của cửa sổ
+				-- },
+				-- },
+			})
+			vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>:ToggleTermToggleAll<CR>", { noremap = true })
+		end,
+	},
 }
