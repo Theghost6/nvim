5a7f22dd75096f5dbc2fd35a8e4f1cd3015bdbe9
Theghost6 <nguyensinh15082004@gmail.com>
2025-04-07 14:36:24 +0700

    update

--- lua/z/plugins/lsp/lspconfig.lua
+++ lua/z/plugins/lsp/lspconfig.lua
@@ -2,17 +2,18 @@ return {
 	"neovim/nvim-lspconfig",
 	-- event = { "BufReadPost", "BufNewFile" },
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"nvimdev/lspsaga.nvim",
-		"ray-x/lsp_signature.nvim",
+		-- "hrsh7th/cmp-nvim-lsp",
+		-- "nvimdev/lspsaga.nvim",
+		-- "ray-x/lsp_signature.nvim",
 		-- { "antosha417/nvim-lsp-file-operations", config = true },
+		"saghen/blink.cmp",
 	},
 	config = function()
 		-- import lspconfig plugin
 		local lspconfig = require("lspconfig")
 
 		-- import cmp-nvim-lsp plugin
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		-- local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		local keymap = vim.keymap -- for conciseness
 
@@ -62,15 +63,27 @@ return {
 		end
 
 		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+		-- local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
 		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		-- for type, icon in pairs(signs) do
+		-- 	local hl = "DiagnosticSign" .. type
+		-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		-- end
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.HINT] = "",
+					[vim.diagnostic.severity.INFO] = "",
+				},
+			},
+		})
 		--
 		lspconfig["ts_ls"].setup({
 			filetypes = {
@@ -141,7 +154,12 @@ return {
 			on_attach = on_attach,
 			capabilities = capabilities,
 			filetypes = {
-    "css", "html", "php", "javascriptreact", "typescriptreact",			},
+				"css",
+				"html",
+				"php",
+				"javascriptreact",
+				"typescriptreact",
+			},
 			init_options = {
 				html = {
 					options = {
