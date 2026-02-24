6706f827f0fb57e281061d9e280869589ae5d594
Theghost6 <nguyensinh15082004@gmail.com>
2026-02-24 17:14:20 +0700

    fix: comment

--- lua/z/plugins/autowrite.lua
+++ lua/z/plugins/autowrite.lua
@@ -1,13 +1,11 @@
 return {
-  "Pocco81/auto-save.nvim",
-  config = function()
-    require("auto-save").setup({
-      execution_message = {
-        enabled = false, -- tắt thô
-        message = ""
-      },
-    })
-  end,
+	"Pocco81/auto-save.nvim",
+	config = function()
+		require("auto-save").setup({
+			execution_message = {
+				enabled = false, -- tắt thông báo khi tự động lưu
+				message = "",
+			},
+		})
+	end,
 }
-
-
