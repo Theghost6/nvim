3c92eda1c1a7129ee183f1f10b03ee31292fb001
Theghost6 <nguyensinh15082004@gmail.com>
2026-02-24 17:17:00 +0700

    update: check health startup

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
 require("z.config.options")
 require("z.config.keymap")
 require("z.lazy")
+
+-- Kiểm tra dependencies (Node, Python, GCC, v.v...) của OS khi chạy nvim trên máy mới
+require("z.config.health")
