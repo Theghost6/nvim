0d24bb82f902304482d1a7d148e84e3909f65b77
Theghost6 <nguyensinh15082004@gmail.com>
2026-02-24 17:15:56 +0700

    change color cusor

--- lua/z/plugins/smear-cursor.lua
+++ lua/z/plugins/smear-cursor.lua
@@ -1,10 +1,12 @@
 return {
 	"sphamba/smear-cursor.nvim",
 	opts = {
-		cursor_color = "#FF8C00",
+		cursor_color = "#A9A9A9", -- DarkGray for ghostly look
+		normal_bg = "#2F2F2F", -- Bóng của con trỏ hòa tan vào nền xám
+		smear_between_neighbor_lines = false, 
 		stiffness = 0.3,
 		trailing_stiffness = 0.1,
-		trailing_exponent = 5,
+		trailing_exponent = 4, -- Làm vệt bóng dài và mờ tự nhiên hơn tí
 		hide_target_hack = true,
 		gamma = 1,
 	},
